579b3cb51b68de0cb082fad5dbfdd32215d6a047
Nika-Shiukashvili <122766345+nikusha223@users.noreply.github.com>
2024-03-12 20:51:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//1st task
+let mynum =100;
+
+if (mynum < 50){ console.log(" ნაკლებია 50ზე")
+}
+ else if( mynum > 20){
+    console.log(" მეტია 20ზე")
+}
+    else{
+        console.log("შეცდომა")
+    }
+
+//2nd task
+let name1 = "მარიამი";
+
+if (name1 = "მარიამი"){
+    console.log("true")
+} else{
+    console.log("false")
+}
+
+//3rd task
+let name2 = "მარიამი";
+
+switch (name2){
+    case 'მარიამი': 
+        console.log('true');
+        break;
+    default:
+        console.log('false');
+}
